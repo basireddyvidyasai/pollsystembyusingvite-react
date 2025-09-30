b5afc9de9f40217f7be75a5a99e13347ea661578
redddy <basivsc100115@gamil.com>
2025-09-30 14:21:38 +0530

    inital commit

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 // Make sure to use your server's address
 // Use 'http://localhost:4000' for local development
-const SERVER_URL = 'http://localhost:4000'; 
-const socket = io(SERVER_URL);
+const PRODUCTION_URL = 'https://backendpolling.onrender.com';
+const socket = io(PRODUCTION_URL);
 
 export default socket;
